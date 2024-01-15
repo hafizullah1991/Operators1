a8e279a174ce2f7837509a12706952eb3bc89dba
Hafiz Hamdard <ahmadshakhi2020@gmail.com>
2024-01-15 09:08:10 -0800

    1excercise operators

---
+++ operators/Program.cs
@@ -0,0 +1,37 @@
+﻿using System.Runtime.Intrinsics.X86;
+
+namespace operators
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+ 
+            int a ;
+            int b ;
+            Console.WriteLine("enter Number1: " );
+            a = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("enter Number1: ");
+            b = int.Parse(Console.ReadLine());
+
+
+            int sum= a + b;
+            Console.WriteLine($"{a} + {b} is  {sum}\n");
+
+
+            //int quotient = a / b;
+            int Quotient = a / b;
+            Console.WriteLine($"{a} / {b} is {Quotient}\n");
+
+
+
+             //int remainder = a % b;
+            int remainder = a % b;
+            Console.WriteLine($"{ a}/{ b} is remainder { remainder}\n");
+
+
+
+        }
+    }
+}
